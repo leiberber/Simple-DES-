4f2a420b82d85e70e735945a2b311d6734a0f999
leiberber <139038523+leiberber@users.noreply.github.com>
2023-09-20 17:05:36 +0800

    Add files via upload

---
+++ untitled3/src/Encipher.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Encipher {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入明文:");
+        String Input = sc.nextLine();
+        //String Ciphertext = Cipher(Input);
+        //System.out.println("加密后密文如下:"+Ciphertext);
+    }
+}
---
+++ untitled3/src/IP.java
@@ -0,0 +1,26 @@
+public class IP {
+    public static String IP(String In) {//初始置换IP
+        int[] IP = {1, 5, 2, 0, 3, 7, 4, 6};//各数字需减去1以对应下标
+        String[] Pro = In.split("");
+        String[] Pro1 = In.split("");//复制一个备份字符串
+        StringBuffer Ciphertext = new StringBuffer("");
+        for (int i = 0; i < 8; i++) {
+            int a = IP[i];
+            Pro[i] = Pro1[a];
+            Ciphertext.append(Pro[i]);
+        }
+        return Ciphertext.toString();//初始置换IP完成
+    }
+
+    public static String IP_1(String In) {//最终置换IP-1
+        int[] IP = {3, 0, 2, 4, 6, 1, 7, 5};//各数字需减去1以对应下标
+        String[] Pro = In.split("");
+        String[] Pro1 = In.split("");//复制一个备份字符串
+        StringBuffer Ciphertext = new StringBuffer();
+        for (int i = 0; i < 8; i++) {
+            Pro[i] = Pro1[IP[i]];
+            Ciphertext.append(Pro[i]);
+        }
+        return Ciphertext.toString();
+    }
+}
