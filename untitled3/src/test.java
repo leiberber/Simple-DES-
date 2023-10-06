b790075879d5e88ffd0270164252012a9adf7c74
leiberber <139038523+leiberber@users.noreply.github.com>
2023-10-06 13:48:03 +0800

    Add files via upload

--- untitled3/src/test.java
+++ untitled3/src/test.java
@@ -9,11 +9,20 @@ public class test {
         System.out.println("解密后明文如下:" + ASCII.asciiDecipher(Ciphertext,Key));
         System.out.println("蛮力破解");
         String[] SameKey = Brute_force.brute_force(Plaintext, Ciphertext);
+
+        long millis1 = System.currentTimeMillis();
+        //计时
+        
+
         for (int i = 0; i < SameKey.length; i++) {
             if (SameKey[i] != null) {
                 System.out.print("检测到密钥" + SameKey[i]);
                 System.out.println(" 明文为" + ASCII.asciiDecipher(Ciphertext, SameKey[i]));
             }
-        } 
+        }
+
+        long millis2 = System.currentTimeMillis();
+        long time = millis2 - millis1;//经过的毫秒数
+        System.out.println("耗时：" + time + "ms");
     }
 }
